d6e3613abaadc2317693b8040d1e9ae61fca2c7c
tomasfqit <tomas.quichimbo@tomebamba.com.ec>
2025-07-12 17:20:13 -0500

    Refactor ESLint configuration and add VSCode settings
    - Updated .eslintrc.cjs and added .eslintrc.js for improved linting rules and support for unused imports.
    - Removed outdated eslint.config.js and updated eslint.config.mjs for better compatibility.
    - Added VSCode settings and extensions recommendations for enhanced development experience.
    - Updated package.json and package-lock.json to include necessary ESLint and TypeScript dependencies.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,31 @@
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:storybook/recommended',
-	],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
-	rules: {
-		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-		'@typescript-eslint/no-non-null-assertion': 'error',
-		'@typescript-eslint/strict-boolean-expressions': 'error',
-	},
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "unused-imports"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-non-null-assertion": "error",
+    "@typescript-eslint/strict-boolean-expressions": "error",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
 };
---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
