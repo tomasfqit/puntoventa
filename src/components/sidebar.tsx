4e616561ebe699641e871323fce0511da2da60f3
tomasfqit <tomas.quichimbo@tomebamba.com.ec>
2025-07-11 23:01:27 -0500

    Enhance Sidebar component layout with improved styling for group titles and item selection. Add visual indicators for active menu items to improve user experience.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -148,8 +148,9 @@ export function Sidebar({ isOpen }: SidebarProps) {
                 <nav className="space-y-4">
                     {filteredMenuData.map((group) => (
                         <div key={group.title}>
-                            <div className="w-full bg-gray-100 p-1 rounded-md">
+                            <div className="w-full flex flex-row gap-2 items-center justify-between p-1 ">
                                 <h3 className="text-xs font-semibold text-gray-500 uppercase">{group.title}</h3>
+                                <div className="h-[1px] bg-gray-100 w-full"></div>
                             </div>
                             <ul className="space-y-1">
                                 {group.items.map((item) => (
@@ -161,6 +162,10 @@ export function Sidebar({ isOpen }: SidebarProps) {
                                                 variant="ghost"
                                                 className="w-full justify-start h-10 px-2 text-gray-700 hover:bg-gray-100"
                                                 onClick={()=> {localPath !== item.path && router.push(item.path)}}
+                                                style={{
+                                                    backgroundColor: localPath === item.path ? "rgba(0, 0, 0, 0.05)" : "transparent",
+                                                    color: localPath === item.path ? "rgba(0, 0, 0, 0.8)" : "rgba(0, 0, 0, 0.6)",
+                                                }}
                                             >
                                                 <item.icon className="mr-3 h-4 w-4" />
                                                 {item.title}
